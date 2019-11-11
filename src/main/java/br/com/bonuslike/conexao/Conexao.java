d1414450841c890eff837ee3985e23d7463e1797
ulissescastro <ulissescastro@10.0.0.103>
2019-11-10 22:57:09 -0300

    ajustes

--- src/main/java/br/com/bonuslike/conexao/Conexao.java
+++ src/main/java/br/com/bonuslike/conexao/Conexao.java
@@ -15,9 +15,9 @@ private static Connection conexao = null;
 	public static Connection getConnection()
 			throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException, NamingException {
 				
-	    String url = "jdbc:mysql://localhost:3306/bonus_like";
-		String user =  "root"; // data.getDataSource("spring.datasource.username"); //"root";
-		String password =  "root"; //data.getDataSource("spring.datasource.password"); //"root";
+	    String url = "jdbc:mysql://s54ham9zz83czkff.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/be1d0afjixihi0rj"; //"jdbc:mysql://localhost:3306/bonus_like";
+		String user =  "udfklj2u5ge947cd"; //"root"; // data.getDataSource("spring.datasource.username"); //"root";
+		String password =  "x21gnwzunigjkzfk"; //data.getDataSource("spring.datasource.password"); //"root";
 
 		Class.forName("com.mysql.cj.jdbc.Driver");
 		conexao  = DriverManager.getConnection(url, user, password);
